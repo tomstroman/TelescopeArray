246014700ea3e787263b6dbfc18b172a1fef0569
Tom Stroman <tom.stroman@gmail.com>
2015-05-21 03:37:58 -0600

    onemodel.C successfully loads an analysis tree into the interpreter's scope.

--- analysis/stereo/onemodel.C
+++ analysis/stereo/onemodel.C
@@ -17,8 +17,24 @@
   TFile *model = new TFile(infile);
   model->GetListOfKeys()->Print();
   
+    // Each species in model has its own TTree with one of these names:
+#define NSPEC 5
+    char treeNames[NSPEC][10] = {"f","g","h","n","he"};  
+    char longTreeNames[NSPEC][32] = {"nature","proton","iron","nitro","helium"};
+    
+  // A single analysis will have up to:
   
+#define NTREE           5 
+
+  // separate TTree objects, indexed by:
   
+#define TUPLE           0
+#define ANALYSIS        1
+#define RECON           2
+#define PROFILE         3
+#define PRA             4
+    
+    
   // USE_PRA tells the code how to incorporate or ignore the
   // third-party pattern-recognition analysis cuts, if available.
   // USE_PRA = 0: do not use
@@ -29,6 +45,9 @@
   
   // now we can load the standard set of quality cuts.
 #include "cuts.h"  
+  
+  
+  
   
   // to save processing time in the future, store one-time analysis results
   // in a complementary file specific to this USE_PRA value. Insert .compN
@@ -49,9 +68,7 @@
     gROOT->LoadMacro("macro/buildTreeAnalysis.C+");
     gROOT->LoadMacro("macro/buildTreeRecon.C");
     
-    // Each species in model has its own TTree with one of these names:
-#define NSPEC 5
-    char treeNames[NSPEC][10] = {"f","g","h","n","he"};
+
     
     for (int s=0; s<NSPEC; s++) {
       // check to see if this species is present, and assign it to t.
@@ -78,8 +95,60 @@
     printf("Done building %s\n",compfile);
   }
   
+
   
   
+
+
+  // Array of TChains, one for each species.
+  TChain *m[NSPEC][NTREE];
   
+  for (int s=0; s<NSPEC; s++) {
+    // grab the main "tuple" tree
+    m[s][TUPLE] = new TChain(treeNames[s]);
+    m[s][TUPLE]->Add(infile);
+    
+    // Go no further if species doesn't exist.
+    // This method will throw an error message if the tree wasn't found,
+    // but it's non-fatal and returns the desired zero.
+    if (m[s][TUPLE]->GetEntries() == 0) {
+      m[s][TUPLE] = NULL;
+      continue;
+    }
+    
+    
+    // and the "profile" tree
+    m[s][PROFILE] = new TChain(Form("%sprof",longTreeNames[s]));
+    m[s][PROFILE]->Add(infile);
+    m[s][TUPLE]->AddFriend(m[s][PROFILE],"p");
+    
+    if (USE_PRA > 0) {
+      // grab PRA if needed/available...
+      m[s][PRA] = new TChain(Form("%spra",treeNames[s]));
+      m[s][PRA]->Add(infile);
+      // ...and link it to TUPLE.
+      m[s][TUPLE]->AddFriend(m[s][PRA],"pra");
+    }
+    
+    // "analysis" tree
+    m[s][ANALYSIS] = new TChain(Form("a%s",treeNames[s]));
+    m[s][ANALYSIS]->Add(compfile);
+    m[s][TUPLE]->AddFriend(m[s][ANALYSIS],"a");
+    
+    // and finally, "recon" tree
+    m[s][RECON] = new TChain(Form("r%s",treeNames[s]));
+    m[s][RECON]->Add(compfile);
+    m[s][TUPLE]->AddFriend(m[s][RECON],"r");
+  }
+  
+  // for convenience, assign shortcuts:
+  // [d]ata, [h]ydrogen, [fe]rrum (iron), [n]itrogen, [he]lium
+  
+  TChain *d = m[0][TUPLE];
+  TChain *h = m[1][TUPLE];
+  TChain *fe = m[2][TUPLE];
+  TChain *n = m[3][TUPLE];
+  TChain *he = m[4][TUPLE];
+
   
 }
