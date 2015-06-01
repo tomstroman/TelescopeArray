c0209c06df8f5469cc3c1e7142df50824ab357d9
Tom Stroman <tom.stroman@gmail.com>
2015-06-01 12:13:01 -0600

    Added stereo_root to ta_common.ta

--- data/ta_common/ta.py
+++ data/ta_common/ta.py
@@ -5,6 +5,9 @@ t0 = 2454282.5 # Julian date of 2007-07-01 00:00:00.0 UTC
 sites = ['brm','lr','md','tale']
 sitenames = ['black-rock','long-ridge','middle-drum','tale']
 
+
+
+
 # absolute-path prefix for permanent storage of various programs' output
 tama = '/tama_' 
 
@@ -17,3 +20,5 @@ localtama = '/scratch1/tama'
 fdgeom = os.getenv('RTDATA') + '/fdgeom'
 geo = [fdgeom + '/geo' + i + '.dst.gz' for 
     i in ['br_joint','lr_joint','md_20131002']]
+    
+stereo_root = '/scratch/tstroman/stereo'    
