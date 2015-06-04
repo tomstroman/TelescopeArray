1bbfe05b82e37365ac55439300708e76c625a4ac
Tom Stroman <tom.stroman@gmail.com>
2015-06-04 15:30:51 -0600

    Added some information for stereo processing

--- data/ta_common/ta.py
+++ data/ta_common/ta.py
@@ -25,3 +25,18 @@ geo = [fdgeom + '/geo' + i + '.dst.gz' for
 stereo_root = '/scratch/tstroman/stereo'    
 stereo_roots = [stereo_root,'/hangar/tstroman/stereo']
 stereo_dates = stereo_root + '/list-of-dates'
+
+# non-participating site in stereo matching.
+# the index is the ID of the missing site or, when all three are present,
+# I've traditionally used -1 or 3 depending on context
+# ...which serendipitously are interchangeable here.
+nps = ['lm','bm','bl','blm']
+
+# participating sites in each combination
+psites = {'lm': ['lr','md'],
+          'bm': ['br','md'],
+          'bl': ['br','lr'],
+          'blm': ['br','lr','md']}
+
+stereo_coincidence_window = 0.002 # seconds
+
