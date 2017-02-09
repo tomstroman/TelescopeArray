0d58e4ba16f1de8c10329e33b8665927685cba77
Tom Stroman <tom.stroman@gmail.com>
2017-02-08 23:20:46 -0700

    Converted Stellarium query to function

--- star_sim/query_stellarium.py
+++ star_sim/query_stellarium.py
@@ -4,54 +4,69 @@
 # with the host system running Stellarium (stellarium.org), with its remote
 # control server interface active and visible to the VM on 192.168.56.1:8090
 
-import httplib, urllib, json, re, sys
+import httplib
+import urllib
+import json
+import re
+import sys
 
-server = '192.168.56.1:8090'
+import argparse
 
-# hard-code this for now. TODO: get this from argument
-jdate = 2451545.0
-object_name = 'alpha boo'
+def query(jdate, object_name):
+    server = '192.168.56.1:8090'
+    conn = httplib.HTTPConnection(server)
+    try:
+      conn.request('GET', '/api/main/status')
+      response = conn.getresponse()
+    except:
+      print 'Stellarium server not detected!'
+      sys.exit(1)
+      
 
-conn = httplib.HTTPConnection(server)
-try:
-  conn.request('GET', '/api/main/status')
-  response = conn.getresponse()
-except:
-  print 'Stellarium server not detected!'
-  sys.exit(1)
-  
+    # Set the time where we want it and pause it there
+    params = urllib.urlencode({'timerate': 0, 'time': jdate})
+    conn.request('POST', '/api/main/time', params)
+    response = conn.getresponse()
+    assert response.status == 200
 
-# Set the time where we want it and pause it there
-params = urllib.urlencode({'timerate': 0, 'time': jdate})
-conn.request('POST', '/api/main/time', params)
-response = conn.getresponse()
-assert response.status == 200
+    # search for the object we want -- and make sure we got results
+    params = urllib.urlencode({'str': object_name})
+    conn.request('GET', '/api/objects/find', params)
+    response = conn.getresponse()
+    assert response.status == 200
 
-# search for the object we want -- and make sure we got results
-params = urllib.urlencode({'str': object_name})
-conn.request('GET', '/api/objects/find', params)
-response = conn.getresponse()
-assert response.status == 200
+    results = json.loads(response.read())
+    assert len(results)
 
-results = json.loads(response.read())
-assert len(results)
+    print u'Searched for {}; found {}'.format(object_name, results[0])
 
-print u'Searched for {}; found {}'.format(object_name, results[0])
+    # Select the object we found
+    params = urllib.urlencode({'target': results[0].encode('utf-8')})
+    conn.request('POST', '/api/main/focus', params)
+    response = conn.getresponse()
+    assert response.status == 200
+    print response.read()
 
-# Select the object we found
-params = urllib.urlencode({'target': results[0].encode('utf-8')})
-conn.request('POST', '/api/main/focus', params)
-response = conn.getresponse()
-assert response.status == 200
-print response.read()
+    # Get the information about the object
+    conn.request('GET', '/api/objects/info')
+    response = conn.getresponse()
+    assert response.status == 200
+    status = response.read()
 
-# Get the information about the object
-conn.request('GET', '/api/objects/info')
-response = conn.getresponse()
-assert response.status == 200
-status = response.read()
+    # use regex to get right ascension and declination
+    a = re.findall("(?<=RA/Dec \(on date\):)(.*)h(.*)m(.*)s/(.*)\xc2\xb0(.*)\'(.*)\"(?=<br>Hour angle)", status)
+    assert len(a) == 1
+    return a[0]
 
-# use regex to get right ascension and declination
-a = re.findall("(?<=RA/Dec \(on date\):)(.*)h(.*)m(.*)s/(.*)\xc2\xb0(.*)\'(.*)\"(?=<br>Hour angle)", status)
-assert len(a) == 1
-print a[0]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('jdate', help='Julian date, e.g. 2451545.000000', type=float)
+    parser.add_argument('name', help='search term for object')
+
+    args = parser.parse_args()
+
+    jdate = args.jdate
+    object_name = args.name
+    
+    print query(jdate, object_name)
