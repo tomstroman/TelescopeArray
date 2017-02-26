32acd490cd8ce314bdccaf8f6bd9bfdeeb8399b2
Tom Stroman <tom.stroman@gmail.com>
2017-02-26 16:00:05 -0700

    Needs polish, but now generate animation frames from observed data

--- star_sim/compare.C
+++ star_sim/compare.C
@@ -17,13 +17,15 @@
 #include "TMath.h"
 #include "TGraph.h"
 #include "TGraph2D.h"
+#include "TPolyLine.h"
+#include "TColor.h"
 
 // Constants
 #define PMTGAP 0.002
 #define PMTPTP 0.0693
 #define PMTFTF 0.06
 #define COS30 0.866025403784438708
-
+#define LOG2 0.693147180559945286
 // Variables for fine-tuning analysis
 #define SEC_PER_BIN 5
 #define INCLUSION_THRESHOLD 0.2
@@ -47,13 +49,15 @@ void fillPmtCoords(double *x, double *y) {
   }
 }
 
-TCanvas *camFace() {
+TCanvas *camFace(TGraph *box = NULL) {
   char canvname[64] = "camface";
   TCanvas *camface = (TCanvas*)gROOT->FindObject(canvname);
   if (!camface) {
     camface = new TCanvas(canvname, "cam", 580 + 28, 505 + 4);
   }
-  TGraph *box = new TGraph(5);
+  if (!box) {
+    box = new TGraph(5);
+  }
   box->SetTitle("Camera face;-x (m);y (m)");
   Double_t wbox = 1.16;
   Double_t hbox = 1.01;
@@ -134,6 +138,14 @@ void analyzeCentroids(TH2F *simpmt, TProfile2D *obspmt, const char* pdfname = "c
   simlight->SetName("simlight");
   simlight->SetTitle("Total signal (simulated);UTC second;signal (arbitrary units)");
   
+  TGraph *simrms = new TGraph(1);
+  simrms->SetName("simrms");
+  simrms->SetTitle("Signal RMS width (simulated);UTC second;meters");
+  
+  TGraph *obsrms = new TGraph(1);
+  obsrms->SetName("obsrms");
+  obsrms->SetTitle("Signal RMS width (observed);UTC second;meters");
+  
   int i, j;
   double x, y, w, sum;
   double ox, oy, ow, osum;
@@ -144,6 +156,8 @@ void analyzeCentroids(TH2F *simpmt, TProfile2D *obspmt, const char* pdfname = "c
   int nbins = simpmt->GetNbinsX();
   int k;
   k = 0;
+  double dist2, sumdist2, osumdist2;
+  double srms, orms;
   for (i=1; i<=nbins; i++) {
     utc_sec = simpmt->GetBinCenter(i);
     printf("bin %d, utc_sec %f\n", i, utc_sec);
@@ -203,6 +217,41 @@ void analyzeCentroids(TH2F *simpmt, TProfile2D *obspmt, const char* pdfname = "c
       ox = oy = 0;
     }
     obscentroid->SetPoint(k, ox, oy/*, utc_sec*/);
+
+    // centroid computed -- now get RMS distance
+    w = ow = 0;
+    sumdist2 = osumdist2 = 0;
+    for (j=1; j<=256; j++) {
+      if (dist[j-1][smaxpmt] < INCLUSION_THRESHOLD) {
+        w = simpmt->GetBinContent(i, j);
+        sum += w;
+        dist2 = (pmtX[j-1] - x)*(pmtX[j-1] - x) + (pmtY[j-1] - y)*(pmtY[j-1] - y);
+        sumdist2 += w * dist2;
+      }
+      if (dist[j-1][omaxpmt] < INCLUSION_THRESHOLD) {
+        ow = obspmt->GetBinContent(i, j);
+        osum += ow;
+        dist2 = (pmtX[j-1] - ox)*(pmtX[j-1] - ox) + (pmtY[j-1] - oy)*(pmtY[j-1] - oy);
+        osumdist2 += ow * dist2;
+      }
+    }
+    
+    if (sum > 0) {
+      sumdist2 /= sum;
+      srms = sqrt(sumdist2);
+    }
+    else {
+      srms = 0;
+    }
+    
+    if (osum > 0) {
+      osumdist2 /= osum;
+      orms = sqrt(osumdist2);
+    }
+    else {
+      orms = 0;
+    }
+      
     
     if (sum > 0 && osum > 0) {
       diffcentroid->SetPoint(k, x-ox, y-oy, utc_sec);
@@ -210,6 +259,8 @@ void analyzeCentroids(TH2F *simpmt, TProfile2D *obspmt, const char* pdfname = "c
       dceny->SetPoint(k, utc_sec, y-oy);
       dcenr->SetPoint(k, utc_sec, TMath::Sqrt((x-ox)*(x-ox) + (y-oy)*(y-oy)));
       dcenf->SetPoint(k, utc_sec, TMath::ATan2(y-oy, x-ox) * TMath::RadToDeg());
+      simrms->SetPoint(k, utc_sec, srms);
+      obsrms->SetPoint(k, utc_sec, orms);
       obslight->SetPoint(k, utc_sec, osum);
       simlight->SetPoint(k, utc_sec, sum);
     }
@@ -232,6 +283,10 @@ void analyzeCentroids(TH2F *simpmt, TProfile2D *obspmt, const char* pdfname = "c
   c1->Print(pdfname);
   dcenf->Draw("ap");
   c1->Print(pdfname);
+  obsrms->Draw("ap");
+  c1->Print(pdfname);
+  simrms->Draw("ap");
+  c1->Print(pdfname);  
   obslight->Draw("ap");
   c1->Print(pdfname);
   simlight->Draw("ap");
@@ -239,6 +294,65 @@ void analyzeCentroids(TH2F *simpmt, TProfile2D *obspmt, const char* pdfname = "c
   
   c1->Close();
 }
+
+void animate(TH2F *simpmt, TProfile2D *obspmt) {
+  TGraph *box = new TGraph(5);
+  TCanvas *camface = camFace(box);
+  
+  int i, j;
+  int nbins = obspmt->GetNbinsX();
+  double maxnpe = obspmt->GetMaximum();
+  double minnpe = obspmt->GetMinimum();
+  double utc_sec;
+  double npe;
+  TPolyLine *pmtpoly[256];
+  Double_t xpmt[7] = {0, COS30, COS30, 0, -COS30, -COS30, 0};
+  Double_t ypmt[7] = {1, 0.5, -0.5, -1, -0.5, 0.5, 1};
+  for (i=0; i<256; i++) {
+    pmtpoly[i] = new TPolyLine(7);
+    for (j=0; j<7; j++) {
+      pmtpoly[i]->SetPoint(j, pmtX[i] + xpmt[j]*0.5*PMTPTP, 
+                              pmtY[i] + ypmt[j]*0.5*PMTPTP);
+      pmtpoly[i]->Draw("f");
+    }
+  }
+  
+  Int_t ncolors = 256;
+  TColor *colors[ncolors];
+  Int_t index;
+  float red, green, blue;
+  Int_t color_base = 20000;
+  for (i=0; i<ncolors; i++) {
+    index = color_base + i;
+    red = green = blue = 1.0 - (float)i/ncolors;
+    colors[i] = new TColor(index, red, green, blue);
+  }
+
+  for (i=1; i<=nbins; i++) {
+    utc_sec = obspmt->GetBinCenter(i);
+    box->SetTitle(Form("%.1f", utc_sec));
+    for (j=0; j<256; j++) {
+      npe = obspmt->GetBinContent(i, j+1);
+      index = (int)(ncolors * (npe - minnpe)/(maxnpe-minnpe));
+      pmtpoly[j]->SetFillColor(color_base + index);
+    }
+    
+    camface->Modified();
+    camface->Update();
+    camface->Print(Form("output/movies/frames/obsframe-lin-%04d.gif", i));
+    
+    for (j=0; j<256; j++) {
+      npe = obspmt->GetBinContent(i, j+1);
+      index = (int)(ncolors * log(1 + (npe-minnpe)/(maxnpe-minnpe))/LOG2);
+      pmtpoly[j]->SetFillColor(color_base + index);
+    }
+    
+    camface->Modified();
+    camface->Update();
+    camface->Print(Form("output/movies/frames/obsframe-log-%04d.gif", i));
+  }
+}
+
 
 void compare(const char *obsfile, const char *simfile, const char *pdfname = NULL) {
   gStyle->SetNumberContours(50);
@@ -282,6 +396,8 @@ void compare(const char *obsfile, const char *simfile, const char *pdfname = NUL
   calculateDistances(pmtX, pmtY, dist);
 
   analyzeCentroids(simpmt, obspmt, pdfname?pdfname:"analyze-centroids.pdf");
+  
+  animate(simpmt, obspmt);
   
 
 
