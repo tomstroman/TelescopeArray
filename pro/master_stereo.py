a9c2b004c5d1b74dd4bfd275ce7990934df3465a
Tom Stroman <tom.stroman@gmail.com>
2017-08-12 10:50:05 -0600

    Initial commit for master stereo file
    - Placeholder code with rudimentary logging

---
+++ pro/master_stereo.py
@@ -0,0 +1,9 @@
+def make_stereo_happen():
+    import logging
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
+    logging.info("stereo happening now")
+    return
+
+if __name__ == '__main__':
+    make_stereo_happen()
+
