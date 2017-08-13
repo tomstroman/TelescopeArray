91f68da6f53b0dbac3717c2a5f8da876cdb94c34
Tom Stroman <tom.stroman@gmail.com>
2017-08-13 15:36:21 -0600

    Add definitions for stereo run tables
    - Sites (duplicate from fadc_tables)
    - Models
    - Species
    - GeometrySets
    - SiteGeometry
    - Calibrations
    - FDPlaneConfigs
    - StereoRuns

--- pro/db/tables.py
+++ pro/db/tables.py
@@ -3,21 +3,36 @@
 # Definitions for database tables
 
 fadc_tables = [
-        ('Sites', 'id INTEGER PRIMARY KEY, shortname TEXT, name TEXT, longname TEXT'),
-        
-        ('Downloads', 'path TEXT PRIMARY KEY, site INTEGER REFERENCES Sites'),
+    ('Sites', 'id INTEGER PRIMARY KEY, shortname TEXT, name TEXT, longname TEXT'),
 
-        ('Runnights', 'date INTEGER, site INTEGER REFERENCES Sites, download TEXT REFERENCES Downloads, logfile TEXT, PRIMARY KEY (date, site)'),
+    ('Downloads', 'path TEXT PRIMARY KEY, site INTEGER REFERENCES Sites'),
 
-        ('Parts', 'part11 INTEGER PRIMARY KEY, date INTEGER, part INTEGER, site INTEGER REFERENCES Sites, daqtrig INTEGER, daqcams INTEGER, daqsigma REAL, FOREIGN KEY (date, site) REFERENCES Runnights(date, site)'),
+    ('Runnights', 'date INTEGER, site INTEGER REFERENCES Sites, download TEXT REFERENCES Downloads, logfile TEXT, PRIMARY KEY (date, site)'),
 
-        ('Filesets', 'part11 INTEGER REFERENCES Parts, ctdprefix TEXT PRIMARY KEY'),
-        ]
+    ('Parts', 'part11 INTEGER PRIMARY KEY, date INTEGER, part INTEGER, site INTEGER REFERENCES Sites, daqtrig INTEGER, daqcams INTEGER, daqsigma REAL, FOREIGN KEY (date, site) REFERENCES Runnights(date, site)'),
+
+    ('Filesets', 'part11 INTEGER REFERENCES Parts, ctdprefix TEXT PRIMARY KEY'),
+]
 
 fadc_process_tables = [
-        ('Parts', 'part INTEGER PRIMARY KEY, daqcams INTEGER NOT NULL, daqtrig INTEGER NOT NULL, ctdtrig INTEGER DEFAULT NULL, badcount INTEGER DEFAULT NULL, dsttrig INTEGER DEFAULT NULL, dstseconds REAL DEFAULT NULL, bytes INTEGER DEFAULT NULL, jstart REAL DEFAULT NULL, pedminutes INTEGER DEFAULT NULL, downcount INTEGER DEFAULT NULL, badcalcount INTEGER DEFAULT NULL'),
-        ]
+    ('Parts', 'part INTEGER PRIMARY KEY, daqcams INTEGER NOT NULL, daqtrig INTEGER NOT NULL, ctdtrig INTEGER DEFAULT NULL, badcount INTEGER DEFAULT NULL, dsttrig INTEGER DEFAULT NULL, dstseconds REAL DEFAULT NULL, bytes INTEGER DEFAULT NULL, jstart REAL DEFAULT NULL, pedminutes INTEGER DEFAULT NULL, downcount INTEGER DEFAULT NULL, badcalcount INTEGER DEFAULT NULL'),
+]
 
 
+stereo_run_tables = [
+    ('Sites', 'id INTEGER PRIMARY KEY, shortname TEXT, name TEXT, longname TEXT'),
 
+    ('Models', 'name TEXT PRIMARY KEY, propername TEXT, dedx_model INTEGER'),
 
+    ('Species', 'corsika_id INTEGER PRIMARY KEY, name TEXT'),
+
+    ('GeometrySets', 'name TEXT PRIMARY KEY, description TEXT'),
+
+    ('SiteGeometry', 'dstfile TEXT PRIMARY KEY, site INTEGER REFERENCES Sites, geometryset TEXT REFERENCES GeometrySets, dstUniqID INTEGER DEFAULT NULL'),
+
+    ('Calibrations', 'version TEXT PRIMARY KEY'),
+
+    ('FDPlaneConfigs', 'name TEXT PRIMARY KEY, path TEXT, calibration TEXT REFERENCES Calibrations, geometryset TEXT REFERENCES GeometrySets'),
+
+    ('StereoRuns', 'name TEXT, path TEXT, fdplaneconfig TEXT REFERENCES FDPlaneConfigs, model TEXT REFERENCES Models, source TEXT'),
+]
