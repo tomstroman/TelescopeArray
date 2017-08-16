6ef3924756e5dea87db4be02ad2cc7c3cfb98f3a
Tom Stroman <tom.stroman@gmail.com>
2017-08-15 22:02:35 -0600

    StereoRun detection/creation for specified name

--- pro/master_stereo.py
+++ pro/master_stereo.py
@@ -2,19 +2,19 @@ from services.stereo_run import StereoRun
 from utils import log
 import logging
 
-def make_stereo_happen(console_mirror=False):
+def make_stereo_happen(console_mirror=False, name=None):
     log_name = log.set_up_log(console_mirror=console_mirror)
     if not console_mirror:
         print 'Logging to', log_name
 
     logging.info("stereo happening now")
 
-    run = StereoRun()
+    run = StereoRun(name)
     run.prepare_stereo_run()
     run.stereo_run()
 
     return run
 
 if __name__ == '__main__':
-    run = make_stereo_happen(console_mirror=True)
+    run = make_stereo_happen(console_mirror=True, name='foo')
 
--- pro/services/stereo_run.py
+++ pro/services/stereo_run.py
@@ -7,7 +7,7 @@ import os
 MASTER_DB_NAME = 'stereo_runs.db'
 
 class StereoRun(object):
-    def __init__(self):
+    def __init__(self, name=None):
         logging.debug("setting up StereoRun")
         self.rootpath = os.getenv('TAFD_STEREO_ROOT')
         if not self.rootpath:
@@ -25,10 +25,11 @@ class StereoRun(object):
         self.db = DatabaseWrapper(db_path)
         self.params = StereoRunParams(self.db)
         logging.info("Parameters object: %s", self.params)
+        self.name = name
 
     def prepare_stereo_run(self):
         logging.warn("method not yet implemented")
-        self.base_run = self._find_or_create_base_run()
+        self.base_run = self._find_or_create_base_run(self.name)
         logging.info("TODO: get global info from db")
         logging.info("TODO: validate parameters")
         logging.info("TODO: prepare paths")
@@ -39,15 +40,42 @@ class StereoRun(object):
     def stereo_run(self):
         logging.warn("method not yet implemented")
 
-    def _find_or_create_base_run(self):
+    def _find_or_create_base_run(self, supplied_name=None):
         sql = 'SELECT name, path FROM StereoRuns WHERE fdplaneconfig=\"{0}\" AND model=\"{1}\"'.format(
                 self.params.fdplane_config,
                 self.params.model,
         )
         logging.debug('sql: %s', sql)
-        runs = self.db.retrieve(sql)
-        for name, path in runs:
+
+        matching_runs = self.db.retrieve(sql)
+
+        name_matches = []
+        for name, path in matching_runs:
             logging.info('found base StereoRun %s at path %s', name, path)
-        if len(runs) == 0:
-            logging.info('No base StereoRuns found')
+            if name == supplied_name:
+                name_matches.append(path)
+        if len(name_matches) == 1:
+            logging.info('One match found; using %s', name_matches[0])
+            return name_matches[0]
+
+        if len(matching_runs) == 0:
+            logging.info('No base StereoRuns found.')
+            if supplied_name:
+                path = os.path.join(supplied_name, self.params.model)
+                logging.info('Attempting to create StereoRun with name=%s, path=%s', supplied_name, path)
+                try:
+                    self.db.insert_row('INSERT INTO StereoRuns VALUES(?, ?, ?, ?)',
+                        (supplied_name, path, self.params.fdplane_config, self.params.model)
+                    )
+                    return path
+                except Exception as err:
+                    logging.error('Failed to create StereoRun. Error: %s', err)
+            else:
+                if len(matching_runs) == 1:
+                    name, path = matching_runs[0]
+                    logging.info('No name supplied, but matching run found at path=%s.', path)
+                    return path
+                else:
+                    logging.error('No valid StereoRuns found and no name supplied for creation.')
+                    raise Exception('No StereoRun')
 
