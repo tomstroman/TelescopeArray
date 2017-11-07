3defdf7fa9017f4ee4b586b75a89e2b955a69973
Tom Stroman <tom.stroman@gmail.com>
2017-11-06 20:49:25 -0700

    Convert make_evt into function

--- pro/step/legacy_stereo/standalone/make_evt.py
+++ pro/step/legacy_stereo/standalone/make_evt.py
@@ -1,7 +1,8 @@
+import argparse
 import os
 import re
-import sys
 
+conv_1mev_to_50kev = (1.0 - 0.05*0.045) / (1.0 - 0.045)
 
 def jdn(ymd, sec):
     year = int(ymd[0:4])
@@ -18,95 +19,95 @@ def jdn(ymd, sec):
         jday -= 1
     return jday, jsec
 
-print sys.argv
+def make_evt(rtsf, evtfile):
+    assert os.path.exists(rtsf), 'Not found: {}'.format(rtsf)
 
-conv_1mev_to_50kev = (1.0 - 0.05*0.045) / (1.0 - 0.045)
+    evt = open(evtfile, 'w')
 
-rtsf = sys.argv[1]
-if not os.path.exists(rtsf):
-    print 'not found: {0}'.format(rtsf)
-    sys.exit(0)
+    print '{0} is open for writing'.format(evtfile)
+    rbname = os.path.basename(rtsf)
+    ymd = rbname[1:5] + rbname[6:8] + rbname[9:11]
 
-evtfile = sys.argv[2]
-evt = open(evtfile, 'w')
+    mdontfile = '{0}/tstroman/md-ontimes.txt'.format(os.getenv('HOME'))
+    with open(mdontfile, 'r') as mdont:
+        matches = []
+        mdontime = mdont.readlines()
+        for line in mdontime:
+            if line[0:8] == ymd:
+                matches.append(line)
 
-print '{0} is open for writing'.format(evtfile)
-rbname = os.path.basename(rtsf)
-ymd = rbname[1:5] + rbname[6:8] + rbname[9:11]
-
-mdontfile = '{0}/tstroman/md-ontimes.txt'.format(os.getenv('HOME'))
-with open(mdontfile, 'r') as mdont:
-    matches = []
-    mdontime = mdont.readlines()
-    for line in mdontime:
-        if line[0:8] == ymd:
-            matches.append(line)
-
-print 'found {0} part(s) (of {3}) on {1} in {2}'.format(
-    len(matches), ymd, mdontfile, len(mdontime)
-)
-if len(matches)==0:
-    exit(0)
-
-rtss = os.popen('$TRUMP/bin/rtsparser.run -gluyspect -trigger {0}'.format(rtsf))
-oldt = ""
-n = 0
-events = rtss.readlines()
-print 'Simulation event count: {0}'.format(len(events))
-for event in events:
-    field = re.split('[\s]+', event.strip())
-    if field[2] != '2' or field[0] == oldt:
-        continue
-
-    oldt = field[0]
-    hour = float(field[7])
-    minute = float(field[8])
-    second = float(field[9])
-    ns = float(field[10])
-
-    t = hour*3600 + minute*60 + second + 1e-9*ns
-
-    for match in matches:
-        lpart = match.split()
-        if float(lpart[2]) <= t < float(lpart[3]):
-            part = lpart[0][8:10]
-            break
-        else:
-            continue
-    # event number, species, ontime flag
-    evt.write('{0} {1} 1\n'.format(n, 1 if field[3] == '14' else 2))
-
-    # MJD, second of Julian day, millisecond, nanosecond
-    jday, jsec = jdn(ymd, t)
-    evt.write('{0} {1} {2} {3}\n'.format(
-        jday - 2440000, int(jsec), int(jsec * 1000), int(ns)
-    ))
-
-    s = -1.0 * (float(field[12]) * float(field[15]) +
-        float(field[13]) * float(field[16])
+    print 'found {0} part(s) (of {3}) on {1} in {2}'.format(
+        len(matches), ymd, mdontfile, len(mdontime)
     )
+    if len(matches)==0:
+        exit(0)
 
-    # components of Rp vector (from CLF?)
-    evt.write('{0} {1} {2}\n'.format(
-        float(field[12]) + s*float(field[15]),
-        float(field[13]) + s*float(field[16]),
-        s*float(field[17])
-    ))
+    rtss = os.popen('$TRUMP/bin/rtsparser.run -gluyspect -trigger {0}'.format(rtsf))
+    oldt = ""
+    n = 0
+    events = rtss.readlines()
+    print 'Simulation event count: {0}'.format(len(events))
+    for event in events:
+        field = re.split('[\s]+', event.strip())
+        if field[2] != '2' or field[0] == oldt:
+            continue
 
-    # components of shower axis unit vector
-    evt.write('{0} {1} {2}\n'.format(field[15], field[16], field[17]))
+        oldt = field[0]
+        hour = float(field[7])
+        minute = float(field[8])
+        second = float(field[9])
+        ns = float(field[10])
 
-    # energy, X0, Xmax, Nmax(50 KeV), Lambda
-    evt.write('{0} {1} {2} {3} {4}\n'.format(
-        1e-18*float(field[11]),
-        field[20],
-        field[22],
-        1e-9*float(field[23])*conv_1mev_to_50kev,
-        field[21]
-    ))
-    n += 1
+        t = hour*3600 + minute*60 + second + 1e-9*ns
 
-evt.close()
-print 'Wrote {0} event{1}.'.format(n, '' if n == 1 else 's')
-if n == 0:
-    os.remove(evtfile)
+        for match in matches:
+            lpart = match.split()
+            if float(lpart[2]) <= t < float(lpart[3]):
+                part = lpart[0][8:10]
+                break
+            else:
+                continue
+        # event number, species, ontime flag
+        evt.write('{0} {1} 1\n'.format(n, 1 if field[3] == '14' else 2))
+
+        # MJD, second of Julian day, millisecond, nanosecond
+        jday, jsec = jdn(ymd, t)
+        evt.write('{0} {1} {2} {3}\n'.format(
+            jday - 2440000, int(jsec), int(jsec * 1000), int(ns)
+        ))
+
+        s = -1.0 * (float(field[12]) * float(field[15]) +
+            float(field[13]) * float(field[16])
+        )
+
+        # components of Rp vector (from CLF?)
+        evt.write('{0} {1} {2}\n'.format(
+            float(field[12]) + s*float(field[15]),
+            float(field[13]) + s*float(field[16]),
+            s*float(field[17])
+        ))
+
+        # components of shower axis unit vector
+        evt.write('{0} {1} {2}\n'.format(field[15], field[16], field[17]))
+
+        # energy, X0, Xmax, Nmax(50 KeV), Lambda
+        evt.write('{0} {1} {2} {3} {4}\n'.format(
+            1e-18*float(field[11]),
+            field[20],
+            field[22],
+            1e-9*float(field[23])*conv_1mev_to_50kev,
+            field[21]
+        ))
+        n += 1
+
+    evt.close()
+    print 'Wrote {0} event{1}.'.format(n, '' if n == 1 else 's')
+    if n == 0:
+        os.remove(evtfile)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('rts_file')
+    parser.add_argument('evt_file')
+    args = parser.parse_args()
+    make_evt(args.rts_file, args.evt_file)
