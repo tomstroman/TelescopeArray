46d6a163bf790fe0b940e597f4c9921131b850e7
Tom Stroman <tom.stroman@gmail.com>
2017-11-07 21:55:53 -0700

    Make "in" file for mc2k12

--- pro/stereo/simulation.py
+++ pro/stereo/simulation.py
@@ -4,6 +4,7 @@
 
 import argparse
 import os
+import re
 
 from glob import glob
 
@@ -24,15 +25,20 @@ def simulate_night(trump_path, geo_files):
         raise Exception('{} TRUMP configurations found (expecting either 1 or 2)'.format(num_configs))
 
     rts = run_trump(trump_path, is_mono, geo_files)
-    prep_md_sim(rts)
-    run_md_sim()
+
+    num_md_events = prep_md_sim(rts)
+
+    if num_md_events > 0:
+        run_md_sim()
 
 
 
 def prep_md_sim(rts):
     evt, num_events = _make_evt(rts)
-    make_in()
-    prepare_path()
+    if num_events > 0:
+        make_in(evt)
+        prepare_path()
+    return num_events
 
 def run_md_sim():
     generate_mc2k12_mc()
@@ -47,8 +53,28 @@ def _make_evt(rts):
     num_events = make_evt(rts, evt)
     return evt, num_events
 
-def make_in():
-    pass
+def make_in(evt):
+    md_in = evt.replace('txt_md.evt', 'txt_md.in')
+# get YYYYMMDD from yYYYYmMMdDDp00.txt_md.evt
+    ymd = ''.join(
+        re.findall(
+            'y([0-9]{4})m([0-9]{2})d([0-9]{2})p00\.txt_md\.evt',
+            os.path.basename(evt)
+        )[0]
+    )
+    in_contents  = 'output file:  ./foo.out\n'
+    in_contents += '            setNr:  {}00\n'.format(ymd)
+    in_contents += '           use DB:  YES\n'
+    in_contents += '            iseed:  -8111111\n'
+    in_contents += '         detector:  ta_md.conf\n'
+    in_contents += '     shift origin:  NO\n'
+    in_contents += '             nevt:  1\n'
+    in_contents += '       event type:  MC_SHOWER\n'
+    in_contents += ' \n'
+    with open(md_in, 'w') as infile:
+        infile.write(in_contents)
+    print 'created', md_in
+
 
 def prepare_path():
     pass
